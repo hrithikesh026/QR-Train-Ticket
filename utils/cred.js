a0d8bdbf88e5dfcd05865b80adfb00ddf4b06c80
Hrithikesh <61539176+hrithikesh026@users.noreply.github.com>
2021-09-03 19:27:43 +0530

    update cred.js

--- utils/cred.js
+++ utils/cred.js
@@ -1,4 +1,4 @@
-db_password = 'CEHWHCqq4ncXOSxk'
-db_username = 'QrTrainTicket'
+db_password = 'Your mongodb password'
+db_username = 'Your mongodb password'
 
 module.exports={db_password,db_username}
